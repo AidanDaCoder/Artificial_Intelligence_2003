25d5f212787c10fb1608ac4e7c1d532ad0c7e87e
Aidan <aidan.chn11@gmail.com>
2026-04-14 17:26:33 +0800

    bfs implemented now ready for testing in gradescope

---
+++ pathfinder.py
@@ -0,0 +1,113 @@
+
+
+def parse_map(file_path):
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+
+    #clean lines (removes \n and extra spaces)
+    lines = [line.strip() for line in lines]
+
+    #first line of rows and cols
+    rows, cols = map(int, lines[0].split())
+
+    #second line start position converting to 0-index for Python indexing
+    row_start, col_start = map(int, lines[1].split())
+    start = (row_start - 1, col_start - 1)
+
+    #third line goal position same conversion as above
+    row_goal, col_goal = map(int, lines[2].split())
+    goal = (row_goal - 1, col_goal - 1)
+    
+    #main grid
+    grid = []
+    for i in range(3, 3 + rows):
+        row = []
+        for coordinate in lines[i].split():
+            if coordinate == 'X':
+                row.append('X')
+            else:
+                row.append(int(coordinate))
+        grid.append(row)
+    
+    return rows, cols, start, goal, grid
+
+
+def get_successors(state, grid):
+    row, col = state
+    successors = []
+
+    #up, down, left, right
+    directions = [
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, 1)
+    ]
+
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for row_direction, col_direction in directions:
+        new_row = row + row_direction
+        new_col = col + col_direction
+
+        if 0 <= new_row < rows and 0 <= new_col < cols:
+            if grid[new_row][new_col] != 'X':
+                successors.append((new_row, new_col))
+        
+    
+    return successors
+
+def bfs(grid, start, goal):
+    queue = []
+    visited = set()
+
+    start_node = {
+        "state": start,
+        "parent": None
+    }
+
+    queue.append(start_node)
+
+    while queue:
+        current_node = queue.pop(0)
+        current_state = current_node["state"]
+        
+        if current_state not in visited:
+            if current_state == goal:
+                return current_node
+            
+            visited.add(current_state)
+
+            for successor in get_successors(current_state, grid):
+                if successor not in visited:
+                    child_node = {
+                        "state": successor,
+                        "parent": current_node
+                    }
+                    queue.append(child_node)
+                    
+    return None
+
+def make_path(goal_node):
+    path = []
+    current = goal_node
+    
+    while current is not None:
+        path.append(current["state"])
+        current = current["parent"]
+    
+    path.reverse()
+    return path
+
+# if __name__ == "__main__":
+#     rows, cols, start, goal, grid = parse_map("test_map.txt")
+
+#     goal_node = bfs(grid, start, goal)
+
+#     if goal_node is None:
+#         print("No path found")
+#     else:
+#         path = reconstruct_path(goal_node)
+#         print("Path found:")
+#         print(path)
